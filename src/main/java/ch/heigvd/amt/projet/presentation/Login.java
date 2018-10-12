95f6dd5a63f5c3ca8c0849b8a4f79c2aecd153b8
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-10-12 11:10:18 +0200

    every features of login page added

--- src/main/java/ch/heigvd/amt/projet/presentation/Login.java
+++ src/main/java/ch/heigvd/amt/projet/presentation/Login.java
@@ -6,9 +6,14 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class Login extends javax.servlet.http.HttpServlet {
+
+    public static final String CREATE_ACCOUNT_VIEW = "";
+    public static final String LOGIN_VIEW = "/WEB-INF/login.jsp";
+    public static final String PASSWORD_FORGOTTEN_VIEW = "";
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -17,7 +22,7 @@ public class Login extends javax.servlet.http.HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+        req.getRequestDispatcher(LOGIN_VIEW).forward(req, resp);
     }
 
     @Override
@@ -25,19 +30,33 @@ public class Login extends javax.servlet.http.HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
 
-        // Verify in DB if email/password are valid
+        /************** LOGIN **************/
+        if (req.getParameter("login") != null) {
+            // Verify in DB if email/password are valid
 
-        //////// IF VALID ////////
-        if (true) {
-            // Create the user with data fetched from DB
-            //User user = getUserFromDB();
-            //req.setAttribute("user", user);
+            //////// IF VALID ////////
+            if (true) {
+                // Create the user with data fetched from DB
+                //User user = getUserFromDB();
+                //req.setAttribute("user", user);
+                HttpSession session = req.getSession();
 
-            this.getServletContext().getRequestDispatcher("/WEB-INF/.jsp").forward(req, resp);
+                this.getServletContext().getRequestDispatcher("/WEB-INF/.jsp").forward(req, resp);
+            }
+            //////// IF INVALID ////////
+            else {
+                this.getServletContext().getRequestDispatcher(LOGIN_VIEW).forward(req, resp);
+            }
         }
-        //////// IF INVALID ////////
-        else {
-            this.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+
+        /************** CREATE ACCOUNT **************/
+        else if (req.getParameter("register_account") != null) {
+            resp.sendRedirect(req.getContextPath() + CREATE_ACCOUNT_VIEW);
+        }
+
+        /************** PASSWORD FORGOTTEN **************/
+        else if (req.getParameter("password_forgotten") != null) {
+            resp.sendRedirect(req.getContextPath() + PASSWORD_FORGOTTEN_VIEW);
         }
     }
 }
