510e01483417ccf63ab19da6d0d94882131aca74
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-11-13 18:40:38 +0100

    AdminServlet + AdminFilter added

---
+++ src/main/java/ch/heigvd/amt/projet/filters/AdminFilter.java
@@ -0,0 +1,38 @@
+package ch.heigvd.amt.projet.filters;
+
+import ch.heigvd.amt.projet.model.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AdminFilter implements Filter {
+    public static final String HOMEPAGE = "/app";
+    public static final String USER_SESSION = "userSession";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        // get session from request
+        HttpSession session = req.getSession();
+
+        if(((User) session.getAttribute(USER_SESSION)).getPermissionsLevel() == 1) {
+            filterChain.doFilter(req, resp);
+        }
+        else {
+            req.getRequestDispatcher(HOMEPAGE).forward(req, resp);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
