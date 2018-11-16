06efab30913cb187a806aca42423ef9ada2c532a
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-11-16 10:35:56 +0100

    my bad

--- amt_gamification/src/main/java/ch/heigvd/amt/projet/business/Constants.java
+++ amt_gamification/src/main/java/ch/heigvd/amt/projet/business/Constants.java
@@ -1,6 +1,6 @@
 package ch.heigvd.amt.projet.business;
 
-public class Constants
+public class Constants {
     public static final int USERS_PER_PAGE = 10;
     public static final int APPS_PER_PAGE  = 10;
 }
