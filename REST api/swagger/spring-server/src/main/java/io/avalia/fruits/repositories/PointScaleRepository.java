eed20f7f9db281b78ad07d204a12c0e4aa57c56e
unknown <olivier.kopp@heig-vd.ch>
2018-12-03 00:02:23 +0100

    debut implementation rest api

---
+++ REST api/swagger/spring-server/src/main/java/io/avalia/fruits/repositories/PointScaleRepository.java
@@ -0,0 +1,10 @@
+package io.avalia.fruits.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Olivier Liechti on 26/07/17.
+ */
+public interface PointScaleRepository extends CrudRepository<FruitEntity, Long>{
+
+}
