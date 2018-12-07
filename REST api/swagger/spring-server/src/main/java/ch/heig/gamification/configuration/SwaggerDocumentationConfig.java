992be088dadb8c4f095f7272d685dd3d08e63a22
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-12-07 15:46:14 +0100

    modif config

--- REST api/swagger/spring-server/src/main/java/ch/heig/gamification/configuration/SwaggerDocumentationConfig.java
+++ REST api/swagger/spring-server/src/main/java/ch/heig/gamification/configuration/SwaggerDocumentationConfig.java
@@ -31,7 +31,7 @@ public class SwaggerDocumentationConfig {
     public Docket customImplementation(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                    .apis(RequestHandlerSelectors.basePackage("io.avalia.gamification.api"))
+                    .apis(RequestHandlerSelectors.basePackage("ch.heig.gamification.api"))
                     .build()
                 .directModelSubstitute(org.joda.time.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(org.joda.time.DateTime.class, java.util.Date.class)
